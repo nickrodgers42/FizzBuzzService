b253ca730298302c771cf28c03b93562f456896f
Nick Rodgers <nick.rodgers42@gmail.com>
2023-05-08 19:26:04 -0400

    Add more fizzbuzz functions

--- packages/server/src/fizzbuzz.ts
+++ packages/server/src/fizzbuzz.ts
@@ -19,6 +19,14 @@ export class FizzBuzz {
         return fizzBuzzString
     }
 
+    public getBetterFizzBuzzString(): string {
+        let fizzBuzzString: string = ""
+        for (let i = 1; i <= 100; ++i) {
+            fizzBuzzString += this.fizzBuzz(i) + " "
+        }
+        return fizzBuzzString
+    }
+
     public fizzBuzz(n: number): string {
         if (n % 15 == 0) {
             return "FizzBuzz"
@@ -30,5 +38,67 @@ export class FizzBuzz {
             return "Buzz"
         }
         return n.toString()
+    }
+
+    public fizzBuzzBazz(n: number): string {
+        const defaultRule = new Rule(() => true, (input: number) => input.toString())
+        const rules: Rule[] = [
+            new ModRule(3, "Fizz"),
+            new ModRule(5, "Buzz"),
+            new ModRule(7, "Bazz"),
+        ]
+        let fizzBuzzString = ""
+        for (const rule of rules) {
+            if (rule.matches(n)) {
+                fizzBuzzString += rule.transform(n)
+            }
+        }
+        if (fizzBuzzString == "") {
+            fizzBuzzString = defaultRule.transform(n)
+        }
+        return fizzBuzzString
+    }
+
+    public fizzBuzzBazz2(n: number): string {
+        const rules: Rule[] = [
+            new Rule((input: number) => (input % 105 == 0), () => "FizzBuzzBazz"),
+            new Rule((input: number) => (input % 35 == 0), () => "BuzzBazz"),
+            new Rule((input: number) => (input % 21 == 0), () => "FizzBazz"),
+            new Rule((input: number) => (input % 15 == 0), () => "FizzBuzz"),
+            new Rule((input: number) => (input % 7 == 0), () => "Bazz"),
+            new Rule((input: number) => (input % 5 == 0), () => "Buzz"),
+            new Rule((input: number) => (input % 3 == 0), () => "Fizz"),
+            new Rule(() => true, (input: number) => input.toString())
+        ]
+        for (const rule of rules) {
+            if (rule.matches(n)) {
+                return rule.transform(n)
+            }
+        }
+        throw new Error("No rule matched")
+    }
+}
+
+export class Rule {
+    private matchingFunction: Function
+    private transformFunction: Function
+
+    constructor(matchingFunction: Function, transformFunction: Function) {
+        this.matchingFunction = matchingFunction
+        this.transformFunction = transformFunction
+    }
+
+    public matches(input: number): boolean {
+        return this.matchingFunction(input)
+    }
+
+    public transform(input: number): string {
+        return this.transformFunction(input)
+    }
+}
+
+export class ModRule extends Rule {
+    constructor(modVal: number, returnValue: string) {
+        super((input: number) => (input % modVal == 0), () => returnValue)
     }
 }
--- packages/server/test/fizzbuzz.test.ts
+++ packages/server/test/fizzbuzz.test.ts
@@ -5,22 +5,96 @@ const fizzBuzz: FizzBuzz = new FizzBuzz()
 // Guiding test
 test("FizzBuzz.getFizzBuzzString returns the correct output", () => {
     const fizzBuzzString: string = fizzBuzz.getFizzBuzzString()
-    expect(fizzBuzzString).toContain("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz")
+    expect(fizzBuzzString).toContain(
+        "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+    )
     expect(fizzBuzzString).toContain("91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz")
 })
 
-test.each([1,2,4])("fizzBuzz(%i) returns a string", (input: number) => {
+test.each([1, 2, 4])("fizzBuzz(%i) returns a string", (input: number) => {
     expect(fizzBuzz.fizzBuzz(input)).toBe(input.toString())
 })
 
-test.each([3,6,9])("fizzBuzz(%i) returns Fizz", (input: number) => {
+test.each([3, 6, 9])("fizzBuzz(%i) returns Fizz", (input: number) => {
     expect(fizzBuzz.fizzBuzz(input)).toBe("Fizz")
 })
 
-test.each([5,10,20])("fizzBuzz(%i) returns Buzz", (input: number) => {
+test.each([5, 10, 20])("fizzBuzz(%i) returns Buzz", (input: number) => {
     expect(fizzBuzz.fizzBuzz(input)).toBe("Buzz")
 })
 
-test.each([15,30,45])("fizzBuzz(%i) returns FizzBuzz", (input: number) => {
+test.each([15, 30, 45])("fizzBuzz(%i) returns FizzBuzz", (input: number) => {
     expect(fizzBuzz.fizzBuzz(input)).toBe("FizzBuzz")
+})
+
+test("FizzBuzz.getBetterFizzBuzzString returns the correct output", () => {
+    const fizzBuzzString: string = fizzBuzz.getBetterFizzBuzzString()
+    expect(fizzBuzzString).toContain(
+        "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
+    )
+    expect(fizzBuzzString).toContain("91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz")
+})
+
+test.each([1, 2, 4])("fizzBuzzBazz(%i) returns a string", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe(input.toString())
+})
+
+test.each([3, 6, 9])("fizzBuzzBazz(%i) returns Fizz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("Fizz")
+})
+
+test.each([5, 10, 20])("fizzBuzzBazz(%i) returns Buzz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("Buzz")
+})
+
+test.each([15, 30, 45])("fizzBuzzBazz(%i) returns FizzBuzz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("FizzBuzz")
+})
+
+test.each([7, 14, 28])("fizzBuzzBazz(%i) returns Bazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("Bazz")
+})
+
+test.each([21, 42, 63])("fizzBuzzBazz(%i) returns FizzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("FizzBazz")
+})
+
+test.each([35, 70, 140])("fizzBuzzBazz(%i) returns BuzzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("BuzzBazz")
+})
+
+test.each([105])("fizzBuzzBazz(%i) returns FizzBuzzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz(input)).toBe("FizzBuzzBazz")
+})
+
+test.each([1, 2, 4])("fizzBuzzBazz2(%i) returns a string", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe(input.toString())
+})
+
+test.each([3, 6, 9])("fizzBuzzBazz2(%i) returns Fizz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("Fizz")
+})
+
+test.each([5, 10, 20])("fizzBuzzBazz2(%i) returns Buzz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("Buzz")
+})
+
+test.each([15, 30, 45])("fizzBuzzBazz2(%i) returns FizzBuzz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("FizzBuzz")
+})
+
+test.each([7, 14, 28])("fizzBuzzBazz2(%i) returns Bazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("Bazz")
+})
+
+test.each([21, 42, 63])("fizzBuzzBazz2(%i) returns FizzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("FizzBazz")
+})
+
+test.each([35, 70, 140])("fizzBuzzBazz2(%i) returns BuzzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("BuzzBazz")
+})
+
+test.each([105])("fizzBuzzBazz2(%i) returns FizzBuzzBazz", (input: number) => {
+    expect(fizzBuzz.fizzBuzzBazz2(input)).toBe("FizzBuzzBazz")
 })
